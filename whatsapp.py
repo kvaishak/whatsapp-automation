737218abdaf8862357045bd4600c9cedc850a324
vaishak.k <vaishak.k@zohocorp.com>
2019-05-05 22:18:24 +0530

    Code for adding multiple contacts added

--- whatsapp.py
+++ whatsapp.py
@@ -8,27 +8,29 @@ import time
 
 # Replace below path with the absolute path
 # to chromedriver in your computer
-driver = webdriver.Chrome('/Users/vaishak-5510/Downloads/chromedriver')
+driver = webdriver.Chrome('/Users/vaishak/Downloads/chromedriver')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
 
 # Replace 'Friend's Name' with the name of your friend
 # or the name of a group
-target = "Friend Name"
+# You can add multiple contacts names inside the array.
+targets = ["Friend_Name1"]
 
 # Replace the below string with your own message
-string = "Hello friend"
-y_arg = '//*[@id="side"]/div[1]/div/label/input'
-input_y = wait.until(EC.presence_of_element_located((
-    By.XPATH, y_arg)))
-input_y.send_keys(target + Keys.ENTER)
+for target in targets :
+    string = "Hello friend"
+    y_arg = '//*[@id="side"]/div[1]/div/label/input'
+    input_y = wait.until(EC.presence_of_element_located((
+                                                         By.XPATH, y_arg)))
+    input_y.send_keys(target + Keys.ENTER)
 
-inp_xpath = "//div[@contenteditable='true']"
-input_box = wait.until(EC.presence_of_element_located((
-    By.XPATH, inp_xpath)))
-for i in range(1):
-    input_box.send_keys(string + Keys.ENTER)
-    time.sleep(1)
+    inp_xpath = "//div[@contenteditable='true']"
+    input_box = wait.until(EC.presence_of_element_located((
+                                                           By.XPATH, inp_xpath)))
+    for i in range(1):
+        input_box.send_keys(string + Keys.ENTER)
+        time.sleep(1)
 
 
